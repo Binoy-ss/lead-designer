b656b6735094bbb9d612652e7e91b9e536a085a2
Developer <test@example.com>
2026-06-01 00:25:27 +0530

    Use Vite default esbuild minifier and update package lock

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,6 @@ export default defineConfig({
     outDir: 'dist',
     emptyOutDir: true,
     sourcemap: false,
-    minify: 'esbuild',
     rollupOptions: {
       output: {
         manualChunks: undefined,
